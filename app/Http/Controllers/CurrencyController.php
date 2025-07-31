b5263e3b61e9e190f69310f45a10a869424986dd
diogogoncalves <dmgdark95@gmail.com>
2025-08-01 00:21:46 +0100

    Update Controllers

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -2,43 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Currency;
+use App\Repositories\CurrencyRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CurrencyController extends Controller
 {
+    private CurrencyRepository $repository;
+
+    public function __construct(CurrencyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index()
     {
-        //
+        // $this->allowedAction('getCurrencies');
+
+        Session::flash('page', 'currencies');
+
+        return view('admin.currencies.index');
     }
 
     public function create()
     {
-        //
+        // $this->allowedAction('getCurrencies');
     }
 
     public function store(Request $request)
     {
-        //
+        // $this->allowedAction('getCurrencies');
     }
 
     public function show(string $id)
     {
-        //
+        // $this->allowedAction('getCurrencies');
     }
 
     public function edit(string $id)
     {
-        //
+        // $this->allowedAction('getCurrencies');
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        // $this->allowedAction('getCurrencies');
     }
 
     public function destroy(string $id)
     {
-        //
+        // $this->allowedAction('getCurrencies');
     }
 }

1f2256604ec8836b348a7dbe391e9977980a5288
diogogoncalves <dmgdark95@gmail.com>
2025-08-01 00:30:25 +0100

    Requests

---
+++ app/Http/Requests/CurrencyRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CurrencyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'name' => 'required|string|max:50',
+            'symbol' => 'required|string|max:50',
+            'rate' => 'nullable|numeric|min:0'
+        ];
+
+        if ($this->get('currency_id'))
+            $rules['code'] = ['required', 'string', Rule::unique('currencies', 'code')->ignore($this->get('currency_id')), 'size:3'];
+        else
+            $rules['code'] = 'required|string|unique:currencies,code|size:3';
+
+
+        return $rules;
+    }
+}
